0f089e895b7879ab992107e11cafd3478baf81dc
Prokuma(平田道也) <Prokuma@users.noreply.github.com>
2016-11-04 17:20:33 +0900

    Solved Some Errors

--- park-001.c
+++ park-001.c
@@ -5,20 +5,20 @@
 #pragma platform(VEX)
 #pragma config(Sensor, dgtl1,  sonarSensor,         sensorSONAR_cm)
 
-#include "Vex_Competition_Include.c"
+#include "Vex_Competition_Includes.c"   //Main competition background code...do not modify!
 
 //Definition of Motor
 #define leftMotor port1
 #define rightMotor port2
 #define leftGrap port3
 #define rightGrap port4
-const char pully {port5, port6, port7, port8, port9, port10}
+const char pullyMotor[6] = {port5, port6, port7, port8, port9, port10};
 
 //Definition of Button
-#define leftGrapButtonIncrease Btn5U
-#define rightGrapButtonIncrease Btn6U
-#define leftGrapButtonDecrease Btn5D
-#define rightGrapButtonDecrease Btn6D
+#define leftGrapButtonIncrese Btn5U
+#define rightGrapButtonIncrese Btn6U
+#define leftGrapButtonDecrese Btn5D
+#define rightGrapButtonDecrese Btn6D```
 #define upPullyButton Btn8R
 #define downPullyButton Btn7L
 
@@ -33,8 +33,8 @@ void pre_auton(){
 
 void pully(int x){
 	int i;
-	for(i = 0; i < 9 ; i++){
-		motor[pully[i]] = x;
+	for(i = 0; i < 5; i++){
+		motor[pullyMotor[i]] = x;
 	}
 }
 
@@ -72,7 +72,7 @@ task usercontrol(){
 		if(vexRT[rightGrapButtonIncrese] == 1){motor[rightGrap] = 30;}else{motor[rightGrap] = 0;}
 		if(vexRT[leftGrapButtonDecrese] == 1){motor[leftGrap] = -30;}else{motor[leftGrap] = 0;}
 		if(vexRT[rightGrapButtonDecrese] == 1){motor[rightGrap] = -30;}else{motor[rightGrap] = 0;}
-		if(vexRT[upPullyButton] == 1){motor[upPully] = 80;}else{motor[upPully] = 0;}
-		if(vexRT[downPullyButton] == 1){motor[downPully] = -80;}else{motor[downPully] = 0;}
+		if(vexRT[upPullyButton] == 1){pully(80);}else{pully(0);}
+		if(vexRT[downPullyButton] == 1){pully(-80);}else{pully(0);}
 	}
 }
